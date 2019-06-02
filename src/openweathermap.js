372f0b5bc25d3b6ed35a8a7e3a199718e7c0b8bb
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-02 18:32:45 +0200

    openweathermap module cleaned up and tested with mocha

--- src/openweathermap.js
+++ src/openweathermap.js
@@ -1,27 +1,12 @@
 const OpenWeatherMap = require('openweathermap-api-module');
-const _ = require('lodash');
 const config = require('./config');
 
-const bbox = {
-  lonLeft: '-180',
-  latBottom: '-90',
-  lonRight: '180',
-  latTop: '90',
-  zoom: '12'
-};
-
-const getWeather = async (cities) => {
+module.exports.getWeather = async (cities) => {
   try {
+    const geonameids = cities.map(city => city.geonameid);
     const client = new OpenWeatherMap(config.openweathermap.key);
-    const geonameids = _.map(cities, city => city.geonameid);
-    const res = await client.currentWeatherByCityIds({ cityIds: geonameids });
-    const weather = res.list;
-    return weather;
+    return await client.currentWeatherByCityIds({ cityIds: geonameids });
   } catch (err) {
     console.log(`getWeather:error:${err}`);
   }
-};
-
-module.exports = {
-  getWeather
 };
--- test/openweathermap.js
+++ test/openweathermap.js
@@ -4,24 +4,28 @@ const { getWeather } = require('../src/openweathermap');
 
 describe('openweathermap', function() {
     describe('getWeather', function() {
+
         let cities = getCities();
-        let weather = null;
+        let data = null;
 
         before(async function() {
-            weather = await getWeather(cities);
+            data = await getWeather(cities);
         });
 
         it('should work', function() {
-            expect(weather).to.be.ok;
+            expect(data).to.be.ok;
         });
 
-        it('should return list', async function() {
-            expect(weather).to.be.an('array');
-            expect(weather.length).to.be.equal(cities.length);
+        it('should return have a list', async function() {
+            expect(data).to.have.property('list');
+            expect(data.list).to.be.an('array');
+            expect(data.list.length).to.be.equal(cities.length);
+            expect(data.cnt).to.be.equal(cities.length);
         });
 
         it('should have a proper data structure', function()  {
-            weather.forEach(city => {
+            expect(data).to.have.property('cnt');
+            data.list.forEach(city => {
                 expect(city).to.have.property('id');
                 expect(city).to.have.nested.property('main.temp');
             });
