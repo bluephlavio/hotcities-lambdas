6992a19772ab19de63006b3dc131439f555a5909
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-04 16:10:57 +0200

    handler function split into fetcher and recorder

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
-const { getAndSaveRecord } = require('./helpers');
+const { getWeather } = require('./openweathermap');
+const { toBeFetchedGeonameids, saveWeatherData, getRecord, saveRecord } = require('./helpers');
 const config = require('./config');
 
 module.exports.fetcher = async () => {
   try {
     await mongoose.connect(config.mongo.connection);
-    const record = await getAndSaveRecord();
-    return record;
+    const geonameids = await toBeFetchedGeonameids();
+    const data = await getWeather(geonameids);
+    await saveWeatherData(data);
   } catch (err) {
     console.log(`fetcher:error:${err}`);
+  }
+};
+
+module.exports.recorder = async () => {
+  try {
+    await mongoose.connect(config.mongo.connection);
+    const record = await getRecord();
+    await saveRecord(record);
+  } catch (err) {
+    console.log(`recorder:error:${err}`);
   }
 };
