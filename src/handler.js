243169f92c7d7b999756c5907544270802a0a200
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-11 09:32:52 +0200

    handler functions restructured and logging implemented

--- src/handler.js
+++ src/handler.js
@@ -4,19 +4,25 @@ const {
   closeDb,
   toBeFetchedGeonameids,
   saveTemperatures,
+  allTemperaturesFetched,
   getRecord,
+  getCityByGeonameid,
   saveRecord
 } = require('./helpers');
+const { MAX_CALLS_PER_MINUTE } = require('./openweathermap');
 
 module.exports.fetcher = async () => {
   try {
     await openDb();
-    const geonameids = await toBeFetchedGeonameids();
-    console.log(geonameids);
+    console.log('Connected to db.');
+    const geonameids = await toBeFetchedGeonameids(MAX_CALLS_PER_MINUTE);
+    console.log(`${geonameids.length} geonameids selected for fetching temperature data.`);
     const temperatures = await getTemperatures(geonameids);
-    console.log(temperatures);
+    console.log(`${temperatures.length} temperature data fetched from openweathermap.`);
     await saveTemperatures(temperatures);
+    console.log('Temperature data saved to db.');
     await closeDb();
+    console.log('Database connection closed.');
   } catch (err) {
     console.log(`fetcher: ${err}`);
   }
@@ -25,9 +31,16 @@ module.exports.fetcher = async () => {
 module.exports.recorder = async () => {
   try {
     await openDb();
-    const record = await getRecord();
-    if (record) {
+    console.log('Connected to db.');
+    const isOk = await allTemperaturesFetched();
+    console.log(`Data ${isOk ? '' : 'not'} fetched for all cities.`);
+    if (isOk) {
+      const record = await getRecord();
+      const { temp, geonameid } = record;
+      const { name } = getCityByGeonameid(geonameid);
+      console.log(`Record temperature found: ${temp} in ${name}!`);
       await saveRecord(record);
+      console.log('Record saved to db.');
     }
     await closeDb();
   } catch (err) {
