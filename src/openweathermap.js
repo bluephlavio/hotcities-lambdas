7957f80444d161ee1d6a4aeaa785f68d0e9224eb
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-12 09:38:53 +0200

    error handling improved in getWeather if something go wrong in the api call

--- src/openweathermap.js
+++ src/openweathermap.js
@@ -9,15 +9,19 @@ const openweathermap = new OpenWeatherMap(config.openweathermap.key);
 module.exports.getWeather = async geonameids => {
   weather = [];
   for (const geonameid of geonameids) {
-    const data = await openweathermap.currentWeatherByCityId({
-      cityId: geonameid,
-      units: 'metric'
-    });
-    weather.push({
-      geonameid,
-      temp: data && data.main ? data.main.temp : null,
-      timestamp: new Date()
-    });
+    try {
+      const data = await openweathermap.currentWeatherByCityId({
+        cityId: geonameid,
+        units: 'metric'
+      });
+      weather.push({
+        geonameid,
+        temp: data && data.main ? data.main.temp : null,
+        timestamp: new Date()
+      });
+    } catch (err) {
+      console.log(`Problem fetching data for ${geonameid}...`);
+    }
   }
   return weather;
 };
