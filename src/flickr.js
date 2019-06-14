10a8bd401c9da87f8f73dee0670d4e4eba368d5f
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-14 08:46:47 +0200

    flickr integration module implemented and tested

---
+++ src/flickr.js
@@ -0,0 +1,135 @@
+const Flickr = require('flickr-sdk');
+const _ = require('lodash');
+const config = require('./config');
+
+const flickr = new Flickr(config.flickr.key);
+
+const taggify = tag => {
+  return tag.toLowerCase().replace(/\s/g, '');
+};
+
+const commonTags = [
+  'panorama',
+  'skyline',
+  'architecture',
+  'building',
+  'skyscraper',
+  'art',
+  'mosque',
+  'masjid',
+  'church',
+  'temple',
+  'street',
+  'park',
+  'bridge',
+  'metro',
+  'station',
+  'square',
+  '-meetings',
+  '-conferences'
+];
+
+const licenses = [
+  { id: '0', name: 'All Rights Reserved', url: '' },
+  { id: '4', name: 'Attribution License', url: 'https://creativecommons.org/licenses/by/2.0/' },
+  {
+    id: '6',
+    name: 'Attribution-NoDerivs License',
+    url: 'https://creativecommons.org/licenses/by-nd/2.0/'
+  },
+  {
+    id: '3',
+    name: 'Attribution-NonCommercial-NoDerivs License',
+    url: 'https://creativecommons.org/licenses/by-nc-nd/2.0/'
+  },
+  {
+    id: '2',
+    name: 'Attribution-NonCommercial License',
+    url: 'https://creativecommons.org/licenses/by-nc/2.0/'
+  },
+  {
+    id: '1',
+    name: 'Attribution-NonCommercial-ShareAlike License',
+    url: 'https://creativecommons.org/licenses/by-nc-sa/2.0/'
+  },
+  {
+    id: '5',
+    name: 'Attribution-ShareAlike License',
+    url: 'https://creativecommons.org/licenses/by-sa/2.0/'
+  },
+  {
+    id: '7',
+    name: 'No known copyright restrictions',
+    url: 'https://www.flickr.com/commons/usage/'
+  },
+  { id: '8', name: 'United States Government Work', url: 'http://www.usa.gov/copyright.shtml' },
+  {
+    id: '9',
+    name: 'Public Domain Dedication (CC0)',
+    url: 'https://creativecommons.org/publicdomain/zero/1.0/'
+  },
+  {
+    id: '10',
+    name: 'Public Domain Mark',
+    url: 'https://creativecommons.org/publicdomain/mark/1.0/'
+  }
+];
+
+const okLicenses = [1, 2, 4, 5, 7, 9, 10];
+
+const getLicenseById = id => {
+  return _.find(licenses, license => license.id == id);
+};
+
+const getUserUrl = id => {
+  return `https://www.flickr.com/people/${id}`;
+};
+
+const searchPhotos = async (city, n = 100) => {
+  const { lat, lng: lon, name, geonameid } = city;
+  const res = await flickr.photos.search({
+    format: 'json',
+    license: okLicenses.join(','),
+    sort: 'relevance',
+    per_page: n,
+    has_geo: '1',
+    lat,
+    lon,
+    radius: 30,
+    tags: _.chain(commonTags)
+      .sampleSize(10)
+      .concat([name])
+      .map(taggify)
+      .value()
+      .join(','),
+    extras: 'url_l,owner_name,license'
+  });
+  const photos = res.body.photos.photo;
+  return _.chain(photos)
+    .map(({ license: licenseid, ...rest }) => {
+      const { name, url } = getLicenseById(licenseid);
+      return {
+        license: { name, url },
+        ...rest
+      };
+    })
+    .map(({ owner: ownerid, ownername: name, ...rest }) => ({
+      owner: {
+        name,
+        url: getUserUrl(ownerid)
+      },
+      ...rest
+    }))
+    .map(({ url_l: url, title, license, owner }) => ({
+      geonameid,
+      url,
+      title,
+      license,
+      owner
+    }))
+    .value();
+};
+
+module.exports = {
+  searchPhotos
+};
---
+++ test/flickr.test.js
@@ -0,0 +1,38 @@
+const { expect } = require('chai');
+const { searchPhotos } = require('../src/flickr');
+const { getCities } = require('./data/factory');
+
+describe('flickr', function() {
+  describe('searchPhotos', function() {
+    const cities = getCities();
+    it('should return a list', async function() {
+      for (const city of cities) {
+        const photos = await searchPhotos(city, 3);
+        expect(photos).to.be.an('array');
+      }
+    });
+
+    it('all elements in the list must have expected fields', async function() {
+      for (const city of cities) {
+        const photos = await searchPhotos(city, 3);
+        expect(photos).to.be.an('array');
+        photos.forEach(photo => {
+          expect(photo).to.have.property('geonameid');
+          expect(photo).to.have.property('url');
+          expect(photo).to.have.property('title');
+          expect(photo).to.have.nested.property('license.name');
+          expect(photo).to.have.nested.property('license.url');
+          expect(photo).to.have.nested.property('owner.name');
+          expect(photo).to.have.nested.property('owner.url');
+          expect(photo.geonameid).to.be.a('number');
+          expect(photo.url).to.be.a('string');
+          expect(photo.title).to.be.a('string');
+          expect(photo.license.name).to.be.a('string');
+          expect(photo.license.url).to.be.a('string');
+          expect(photo.owner.name).to.be.a('string');
+          expect(photo.owner.url).to.be.a('string');
+        });
+      }
+    });
+  });
+});
