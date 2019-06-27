4747d5f53de422dec1d8e56f12e171e738314657
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-27 09:04:20 +0200

    removed __v field from records/current endpoint

--- src/controllers/common.js
+++ src/controllers/common.js
@@ -19,7 +19,7 @@ module.exports.list = model => async (req, res, next) => {
 module.exports.get = model => async (req, res, next) => {
   try {
     const match = res.match || {};
-    const data = await model.findOne(match);
+    const data = await model.findOne(match).select(['-__v']);
     if (!data) return next({ message: 'Not found.', code: 404 });
     return res.status(200).json({ data });
   } catch (err) {
--- src/controllers/records.js
+++ src/controllers/records.js
@@ -2,7 +2,10 @@ const Record = require('../models/record');
 
 module.exports.current = () => async (req, res, next) => {
   try {
-    const data = await Record.last();
+    const data = await Record.findOne()
+      .sort('-timestamp')
+      .select(['-__v']);
+    if (!data) return next({ massage: 'Not found.', code: 404 });
     res.status(200).send({ data });
   } catch (err) {
     next(err);
