a6d841c81b9014d7035619556914660f22fc0b2d
Flavio Grandin <flavio.grandin@gmail.com>
2022-06-19 10:27:19 +0200

    feat(score): minor change to score algorithm

--- src/models/record.js
+++ src/models/record.js
@@ -4,80 +4,80 @@ const _ = require('lodash');
 const RecordSchema = new mongoose.Schema({
   geonameid: {
     type: Number,
-    required: true
+    required: true,
   },
   temp: {
     type: Number,
-    required: true
+    required: true,
   },
   timestamp: {
     type: Date,
     default: Date.now,
-    required: true
-  }
+    required: true,
+  },
 });
 
-RecordSchema.statics.current = async function() {
+RecordSchema.statics.current = async function () {
   return await this.findOne().sort({ timestamp: 'desc' });
 };
 
-RecordSchema.statics.first = async function() {
+RecordSchema.statics.first = async function () {
   return await this.findOne().sort({ timestamp: 'asc' });
 };
 
-RecordSchema.statics.last = async function() {
+RecordSchema.statics.last = async function () {
   return await this.findOne().sort({ timestamp: 'desc' });
 };
 
-RecordSchema.statics.startTime = async function() {
+RecordSchema.statics.startTime = async function () {
   const first = await this.first();
   const { timestamp } = first;
   return timestamp;
 };
 
-RecordSchema.statics.stopTime = async function() {
+RecordSchema.statics.stopTime = async function () {
   const last = await this.last();
   const { timestamp } = last;
   return timestamp;
 };
 
-RecordSchema.statics.hottest = async function() {
+RecordSchema.statics.hottest = async function () {
   return await this.findOne().sort({ temp: 'desc' });
 };
 
-RecordSchema.statics.coolest = async function() {
+RecordSchema.statics.coolest = async function () {
   return await this.findOne().sort({ temp: 'asc' });
 };
 
-RecordSchema.statics.tempRange = async function() {
+RecordSchema.statics.tempRange = async function () {
   const { temp: minTemp } = await this.coolest();
   const { temp: maxTemp } = await this.hottest();
   return [minTemp, maxTemp];
 };
 
-RecordSchema.statics.ranking = async function() {
+RecordSchema.statics.ranking = async function () {
   const count = await this.countDocuments();
   const data = await this.aggregate()
     .group({
       _id: '$geonameid',
       recordfrac: { $sum: 1 / count },
-      recordtemp: { $max: '$temp' }
+      recordtemp: { $max: '$temp' },
     })
     .lookup({
       from: 'cities',
       localField: '_id',
       foreignField: 'geonameid',
-      as: 'city'
+      as: 'city',
     })
     .unwind('$city')
     .addFields({
       'city.recordfrac': '$recordfrac',
-      'city.recordtemp': '$recordtemp'
+      'city.recordtemp': '$recordtemp',
     })
     .replaceRoot('$city')
     .exec();
-  const recordfracs = data.map(item => item.recordfrac);
-  // const maxRecordFrac = _.max(recordfracs);
+  const recordfracs = data.map((item) => item.recordfrac);
+  const maxRecordFrac = _.max(recordfracs);
   const [minTemp, maxTemp] = await this.tempRange();
   const deltaTemp = maxTemp - minTemp;
   return _.chain(data)
@@ -85,14 +85,15 @@ RecordSchema.statics.ranking = async function() {
       ...rest,
       recordfrac,
       recordtemp,
-      score: recordfrac * Math.pow((recordtemp - minTemp) / deltaTemp, 5) * 100
-      // Math.pow(recordfrac / maxRecordFrac, 1) *
-      // Math.pow((recordtemp - minTemp) / (maxTemp - minTemp), 3)
+      score:
+        (recordfrac / maxRecordFrac) *
+        Math.pow((recordtemp - minTemp) / deltaTemp, 5) *
+        100,
     }))
     .orderBy(['score'], ['desc'])
     .map((entry, i) => ({
       ...entry,
-      rank: i + 1
+      rank: i + 1,
     }))
     .value();
 };
