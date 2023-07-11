4f43da201439ac80fb634f1871a6eeaf3d74274c
Flavio Grandin <flavio.grandin@gmail.com>
2023-07-11 17:15:26 +0000

    feat: rest api ported to graphql

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,7 @@
+const { GraphQLScalarType, Kind } = require("graphql");
 const City = require("../models/city");
 const Record = require("../models/record");
+const State = require("../models/state");
 
 const resolvers = {
   Query: {
@@ -9,10 +11,39 @@ const resolvers = {
     city: async (_, { geonameid }) => {
       return await City.findByGeonameid(geonameid);
     },
-    records: async (_, { limit, match, sort }) => {
-      return await Record.aggregate([
+    records: async (_, { limit, match, sort, startDate, endDate, period }) => {
+      const matchObj = match ? JSON.parse(match) : {};
+      if (startDate && endDate) {
+        matchObj.timestamp = {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        };
+      }
+      const groupBy = {
+        day: { $dateToString: { format: "%Y-%m-%d", date: "$timestamp" } },
+        week: { $dateToString: { format: "%Y-%U", date: "$timestamp" } },
+        month: { $dateToString: { format: "%Y-%m", date: "$timestamp" } },
+      };
+      const result = await Record.aggregate([
         {
-          $match: match ? JSON.parse(match) : {},
+          $match: matchObj,
+        },
+        {
+          $group: {
+            _id: period ? groupBy[period] : { timestamp: "$timestamp" },
+            record: {
+              $max: {
+                temp: "$temp",
+                timestamp: "$timestamp",
+                geonameid: "$geonameid",
+              },
+            },
+          },
+        },
+        {
+          $replaceRoot: {
+            newRoot: "$record",
+          },
         },
         {
           $lookup: {
@@ -29,11 +60,55 @@ const resolvers = {
           $sort: sort ? JSON.parse(sort) : {},
         },
         {
-          $limit: limit || 0,
+          $limit: limit,
         },
       ]);
+      return result;
+    },
+    currentRecord: async () => {
+      const results = await Record.aggregate([
+        {
+          $sort: { timestamp: -1 },
+        },
+        {
+          $limit: 1,
+        },
+        {
+          $lookup: {
+            from: "cities",
+            localField: "geonameid",
+            foreignField: "geonameid",
+            as: "city",
+          },
+        },
+        {
+          $unwind: "$city",
+        },
+      ]);
+      return results[0];
+    },
+    state: async () => {
+      const result = await State.findOne();
+      return result;
     },
   },
+
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Date custom scalar type",
+    parseValue: (value) => {
+      return new Date(value);
+    },
+    serialize: (value) => {
+      return value.toISOString();
+    },
+    parseLiteral: (ast) => {
+      if (ast.kind === Kind.STRING) {
+        return new Date(ast.value);
+      }
+      return null;
+    },
+  }),
 
   City: {},
 
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -2,10 +2,21 @@ const { makeExecutableSchema } = require("@graphql-tools/schema");
 const resolvers = require("./resolvers");
 
 const typeDefs = `
+  scalar Date
+  
   type Query {
     cities: [City]
     city(geonameid: Int): City
-    records(limit: Int, match: String, sort: String): [Record]
+    records(
+      limit: Int = 1000000,
+      match: String = null,
+      sort: String = "{ \\"timestamp\\": -1 }",
+      startDate: Date = null,
+      endDate: Date = null,
+      period: String = null
+    ): [Record]
+    currentRecord: Record
+    state: State
   }
 
   type City {
@@ -23,10 +34,55 @@ const typeDefs = `
   }
 
   type Record {
-    _id: ID!,
+    _id: ID!
     city: City!
     temp: Float!
-    timestamp: String!
+    timestamp: Date!
+  }
+
+  type Current {
+    _id: ID!
+    geonameid: Int!
+    name: String!
+    localname: String!
+    lng: Float!
+    lat: Float!
+    countryname: String!
+    countrycode: String!
+    population: Int!
+    temp: Float!
+    timestamp: Date!
+    score: Float!
+    recordfrac: Float!
+    recordtemp: Float!
+    rank: Int!
+  }
+
+  type RankingItem {
+    _id: ID!
+    geonameid: Int!
+    name: String!
+    localname: String!
+    lng: Float!
+    lat: Float!
+    countryname: String!
+    countrycode: String!
+    population: Float!
+    score: Float!
+    recordfrac: Float!
+    recordtemp: Float!
+    rank: Int!
+  }
+
+  type Stats {
+    ranking: [RankingItem]
+    temprange: [Float]
+  }
+
+  type State {
+    _id: ID!
+    current: Current
+    stats: Stats
   }
 `;
 
